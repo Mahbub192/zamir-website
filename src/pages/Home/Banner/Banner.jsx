bdc4d70993643ca7d203ca5bbab8de82a9f3442b
unknown <mahabubaligub192@gmail.com>
2023-10-05 12:53:14 +0600

    naveBar and Banner completed

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,16 @@
+const Banner = () => {
+  return (
+    <div className=" h-[850px] bg-gradient-to-r from-cyan-600 to-blue-600 text-center pt-48">
+      <h1 className="text-5xl font-bold text-gray-200">Quality Digital Marketing Service <br /> You Really Want!</h1>
+      <p className="text-lg font-bold text-gray-200 mt-10">
+        See exactly what we’d do, how much we’d change, and why your <br /> competitors
+        are in deep trouble
+      </p>
+      <div className="flex items-center justify-center mt-10 ">
+        <button className="text-white border-2 px-6 py-3 rounded-lg text-xl font-bold hover:text-gray-700">Contact Us</button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
