030ae532205415e753f54a842655ab5f13f7b95c
unknown <mahabubaligub192@gmail.com>
2023-11-11 16:00:19 +0600

    change testimonials section

--- src/pages/Home/MyInfo/MyInfo.jsx
+++ src/pages/Home/MyInfo/MyInfo.jsx
@@ -110,9 +110,12 @@ const MyInfo = () => {
                   <span className="text-red-600">Message is required</span>
                 )}
               </div>
+              <div className="mt-5">
+                <p className="text-center text-gray-500">Your information is 100% secure.</p>
+              </div>
 
               <button
-                className={`w-full mt-10 mx-auto border-2 px-10 py-2 text-lg md:text-xl font-bold bg-blue-500 hover:bg-blue-800 text-white `}
+                className={`w-full mt-5 mx-auto border-2 px-10 py-2 text-lg md:text-xl font-bold bg-blue-500 hover:bg-blue-800 text-white `}
                 type="submit"
               >
                 Submit
